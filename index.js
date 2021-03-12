3e05f60b31abc30adf2e9f9f438e21eda1e48912
comeevery-git <comeevery@gmail.com>
2021-03-12 23:17:40 +0900

    cronjob fix - kor ver

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const send = async(message) => {
     });
 }
 
-schedule.scheduleJob('05 11,23 * * 1-7', function(){
+schedule.scheduleJob('25 02,14 * * 1-7', function(){
     send('같이 개발 공부하자 😉');
 });
-schedule.scheduleJob('50 8 * * 1-5', function(){
+schedule.scheduleJob('* 00 * * 1-5', function(){
     send('출근!');
 });
-schedule.scheduleJob('5 9 * * 1-5', function(){
+schedule.scheduleJob('* 8 * * 1-5', function(){
     send('퇴근!');
 });
 // schedule end
