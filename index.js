ba00828fdd341526bac34bf790b8b6aa1569bff4
comeevery-git <comeevery@gmail.com>
2021-03-12 16:25:05 +0900

    slack event function

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+// OAuth & Permissions 설정 페이지에서 생성된 Bot User OAuth Access Token
+const token = 'xoxb-1874615282960-1856981463604-V6B2GKmOwH7nNAi3PPdjTy6y'; 
+const web = new WebClient(token);
+
+const { WebClient } = require('@slack/web-api');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.json()) .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post('/slack/events',
+    (req, res) => {
+      let body = req.body; let event = body.event;
+      if(body.type === 'event_callback') {
+        if(event.type === 'message') {
+          // 메시지 이벤트인 경우, 메시지가 '안녕'이면 '안녕하세요' 메시지 전송 
+          if(event.text === '안녕') { 
+            console.log(`인사 메시지 수신 channel:${event.channel}, user:${event.user}`); 
+            web.chat.postMessage({
+              channel: event.channel, text: '안녕하세요.'
+            }).then(result => {
+              console.log('Message sent: ' + result.ts)
+            });
+            res.sendStatus(200);
+          }
+        }
+      } else if(body.type === 'url_verification') {
+        // URL 검증을 위한 처리
+        console.log('url verification')
+        res.send(body.challenge); 
+      } else {
+        res.sendStatus(200); 
+      } 
+    }) .listen(PORT, () => console.log(`Listening on ${ PORT }`))
